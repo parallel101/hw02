326c788bdc1de684edd3b643acf82c387c9675c2
yanmulin <yans1996@outlook.com>
2021-12-19 02:22:49 +0000

    switch to unique_ptr

--- main.cpp
+++ main.cpp
@@ -3,29 +3,30 @@
 #include <memory>
 
 struct Node {
-    // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
-    // 如果能改成 unique_ptr 就更好了!
+    // 这两个指针会造成什么问题？请修复 -> 两个shared_ptr前后节点循环引用
+    std::unique_ptr<Node> next;
+    Node* prev;
+    // 如果能改成 unique_ptr 就更好了! -> 每个Node的生命周期由上一个节点管理
 
     int value;
 
-    Node(int value) : value(value) {}  // 有什么可以改进的？
+    Node(int value) : value(value) {}  // 有什么可以改进的？-> 声明为explicit 避免implicit casting
 
     void insert(int value) {
-        auto node = std::make_shared<Node>(value);
+        auto node = std::make_unique<Node>(value);
         node->value = value;
-        node->next = next;
+        node->next = std::move(next);
         node->prev = prev;
-        if (prev)
-            prev->next = node;
         if (next)
-            next->prev = node;
+            next->prev = node.get();
+        if (prev)
+            prev->next = std::move(node);
+        // 当前节点 释放？
     }
 
     void erase() {
         if (prev)
-            prev->next = next;
+            prev->next = std::move(next);
         if (next)
             next->prev = prev;
     }
@@ -36,49 +37,53 @@ struct Node {
 };
 
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node> head;
 
     List() = default;
 
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        // head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
     }
 
-    List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
+    List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？ 
+    // -> l1=l2 => l1=List(l2), 先调用拷贝构造函数，然后调用移动复制函数
 
     List(List &&) = default;
     List &operator=(List &&) = default;
 
     Node *front() const {
+        // TODO: 边界检查
         return head.get();
     }
 
     int pop_front() {
+        // TODO:边界检查
         int ret = head->value;
-        head = head->next;
+        head = std::move(head->next);
         return ret;
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+        auto node = std::make_unique<Node>(value);
         if (head)
-            head->prev = node;
-        head = node;
+            head->prev = node.get();
+        node->next = std::move(head);
+        head = std::move(node);
     }
 
     Node *at(size_t index) const {
         auto curr = front();
         for (size_t i = 0; i < index; i++) {
+            // TODO: 边界检查
             curr = curr->next.get();
         }
         return curr;
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(List lst) {  // 有什么值得改进的？-> 传递引用常量，避免拷贝
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
