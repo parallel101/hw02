d0ce505ea880f7ab9b9a2425db3d60802d07855d
Derived Cat <hooyuser@outlook.com>
2021-12-20 07:52:47 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-@echo off
-IF NOT EXIST "build" mkdir "build"
-cd build
-cmake ..
+cmake -B ./build -DCMAKE_BUILD_TYPE=Release
+cmake --build ./build --target ALL_BUILD --config Release
+.\build\bin\main.exe
+pause
