2251f5e9e89345bf1a02c9edcb931ef1a235c173
空腹不能吃饭 <522724237@qq.com>
2021-12-19 16:28:11 +0800

    use unique ptr implement deep copy and implement template

--- main.cpp
+++ main.cpp
@@ -2,50 +2,41 @@
 #include <cstdio>
 #include <memory>
 
+template <typename T>
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::weak_ptr<Node> prev;
-    // 如果能改成 unique_ptr 就更好了!
-
     std::unique_ptr<Node> next;
     Node* prev;
+    // 如果能改成 unique_ptr 就更好了!
 
-    int value;
+    T value;
 
-    explicit Node(int value) : value(value) {}  // 有什么可以改进的？
-
-    void insert(int value) {
-        auto node = std::make_shared<Node>(value);
+    explicit Node(T value) : value(value), prev(nullptr){}  // 有什么可以改进的？
+ 
+    void insert(T value) {
+        auto node = std::make_unique<Node>(value);
         node->value = value;
-        node->next = next;
+        node->next = std::move(next);
         node->prev = prev;
-        if (!prev.expired()) {
-            std::shared_ptr<Node> tmp = prev.lock();
-            tmp->next = node;
-        }
 
         if (next)
-            next->prev = node;
-    }
-
-    void erase() {
-        if (!prev.expired()) {
-            std::shared_ptr<Node> tmp = prev.lock();
-            tmp->next = next;
+            next->prev = node.get();
+        
+        if (prev) {
+            prev->next = std::move(node);
         }
-
-        if (next)
-            next->prev = prev;
     }
+
 
     ~Node() {
         printf("~Node()\n");   // 应输出多少次？为什么少了？
     }
 };
 
+template <typename T>
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node<T>> head;
+    size_t count = 0;
 
     List() = default;
 
@@ -53,10 +44,9 @@ struct List {
         printf("List 被拷贝！\n");
         // head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
-        auto ohead = other.head;
-        head = ohead;
-        while(ohead->next) {
-            ohead = ohead->next;
+        auto num = other.count;
+        while(num--) {
+            push_front(other.at(num)->value);
         }
     }
 
@@ -65,34 +55,48 @@ struct List {
     List(List &&) = default;
     List &operator=(List &&) = default;
 
-    Node *front() const {
+    Node<T> *front() const {
         return head.get();
     }
 
-    int pop_front() {
-        int ret = head->value;
-        head = head->next;
+    T pop_front() {
+        T ret = head->value;
+        head = std::move(head->next);
+        count--;
         return ret;
     }
 
-    void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+    void push_front(T value) {
+        auto node = std::make_unique<Node<T>>(value);
         if (head)
-            head->prev = node;
-        head = node;
+            head->prev = node.get();
+        node->next = std::move(head);
+        head = std::move(node);
+        count++;
     }
 
-    Node *at(size_t index) const {
+    Node<T> *at(size_t index) const {
         auto curr = front();
         for (size_t i = 0; i < index; i++) {
             curr = curr->next.get();
         }
         return curr;
     }
+
+    void erase(size_t index) {
+        count--;
+        auto tmp = at(index);
+        if (tmp->next)
+            tmp->next->prev = tmp->prev;
+
+        if (tmp->prev) {
+            tmp->prev->next = std::move(tmp->next);
+        }
+    }
 };
 
-void print(const List &lst) {  // 有什么值得改进的？
+template <typename T>
+void print(const List<T> &lst) {  // 有什么值得改进的？
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
@@ -101,7 +105,7 @@ void print(const List &lst) {  // 有什么值得改进的？
 }
 
 int main() {
-    List a;
+    List<int> a;
 
     a.push_front(7);
     a.push_front(5);
@@ -113,13 +117,13 @@ int main() {
 
     print(a);   // [ 1 4 9 2 8 5 7 ]
 
-    a.at(2)->erase();
+    a.erase(2);
 
     print(a);   // [ 1 4 2 8 5 7 ]
 
-    List b = a;
+    List<int> b = a;
 
-    a.at(3)->erase();
+    a.erase(3);
 
     print(a);   // [ 1 4 2 5 7 ]
     print(b);   // [ 1 4 2 8 5 7 ]
