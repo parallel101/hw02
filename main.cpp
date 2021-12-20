f4f462c9793c568f7774f25f35108c1824f125b4
lulongfei <lulongfei@of3d.com>
2021-12-20 21:31:16 +0800

    update assignment code

--- main.cpp
+++ main.cpp
@@ -1,35 +1,44 @@
 /* 基于智能指针实现双向链表 */
 #include <cstdio>
 #include <memory>
+#include <iostream>
 
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    std::unique_ptr<Node> next;
+    Node* prev = nullptr;
     // 如果能改成 unique_ptr 就更好了!
 
     int value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
+    Node(int val) :value(val) {
     }
 
     void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
+        auto newNode = std::make_unique<Node>(val);
+        auto oldNode = std::make_unique<Node>(value);
+
+        newNode->prev = prev;
         if (next)
-            next->prev = node;
+            next->prev = oldNode.get();
+
+        oldNode->next = std::move(next);
+        newNode->next = std::move(oldNode);
+		if (prev)
+            prev->next = std::move(newNode);
+
     }
 
     void erase() {
-        if (prev)
-            prev->next = next;
         if (next)
-            next->prev = prev;
+        {
+			next->prev = prev;
+        }
+        if (prev)
+        {
+			prev->next = std::move(next);
+        }
     }
 
     ~Node() {
@@ -38,14 +47,63 @@ struct Node {
 };
 
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node> head;
 
     List() = default;
 
     List(List const &other) {
-        printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
-        // 请实现拷贝构造函数为 **深拷贝**
+        printf("List copied\n");
+  //      auto newHead = std::make_unique<Node>(other.head->value);
+		//for (auto curr = other.front()->next.get(); curr; curr = curr->next.get()) {
+		//	std::cout << curr->value << std::endl;
+		//	auto newNode = std::make_unique<Node>(curr->value);
+		//	newHead->prev = newNode.get();
+		//	newNode->next = std::move(newHead);
+  //          //newNode->prev = newHead.get();
+  //          //newHead->next = std::move(newNode);
+
+  //          //newNode = std::move(newHead);
+		//	newHead = std::move(newNode);
+
+		//}
+
+  //      Node* headPtr = newHead.get();
+  //      if (headPtr != nullptr)
+  //      {
+		//	while (headPtr->next.get())
+		//	{
+		//		headPtr = headPtr->next.get();
+		//	}
+
+  //      }
+
+  //      std::cout << "---------------" << std::endl;
+  //      std::cout << headPtr << std::endl;
+  //      std::cout << headPtr->value << std::endl;
+  //      std::cout << "---------------" << std::endl;
+
+  //      head.reset(headPtr);
+		auto tempPtr = other.head.get();
+		if (tempPtr != nullptr)
+		{
+			while (tempPtr->next.get())
+			{
+				tempPtr = tempPtr->next.get();
+
+			}
+
+			for (auto curr = tempPtr; curr != other.head.get(); curr = curr->prev)
+			{
+				int _value = curr->value;
+				push_front(_value);
+			}
+
+			push_front(other.head->value);
+		}
+		else
+		{
+			head = nullptr;
+		}
     }
 
     List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
@@ -58,17 +116,24 @@ struct List {
     }
 
     int pop_front() {
-        int ret = head->value;
-        head = head->next;
-        return ret;
+        if (head != nullptr)
+        {
+			int ret = head->value;
+			head = std::move(head->next);
+			return ret;
+
+        }
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+        auto node = std::unique_ptr<Node>(new Node(value));
         if (head)
-            head->prev = node;
-        head = node;
+        {
+            head->prev = node.get();
+        }
+
+        node->next = std::move(head);
+        head = std::move(node);
     }
 
     Node *at(size_t index) const {
@@ -80,7 +145,7 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List& lst) {  // 有什么值得改进的？
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
@@ -108,9 +173,13 @@ int main() {
     List b = a;
 
     a.at(3)->erase();
-
     print(a);   // [ 1 4 2 5 7 ]
+    a.at(3)->insert(90);
+
+    print(a);   // [ 1 4 2 90 5 7 ]
     print(b);   // [ 1 4 2 8 5 7 ]
+    //b.pop_front();
+    //print(b);   // [ 4 2 8 5 7 ]
 
     b = {};
     a = {};
