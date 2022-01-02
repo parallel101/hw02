a4095054e22a6f84ae2f752f9ef7837968935f28
zhuxiaolong <zhuxiaolong_auto@163.com>
2022-01-02 20:29:23 +0800

    shared_ptr and weak_ptr

--- main.cpp
+++ main.cpp
@@ -4,36 +4,36 @@
 
 struct Node {
     // 这两个指针会造成什么问题？请修复
+    // 循环引用问题, 导致指针不会被释放
     std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
-    // 如果能改成 unique_ptr 就更好了!
-
+    std::weak_ptr<Node>   prev;
     int value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
+    // explicit 防止错误的隐式类型转换
+    explicit Node(int val) {
         value = val;
     }
 
     void insert(int val) {
-        auto node = std::make_shared<Node>(val);
+        std::shared_ptr<Node> node = std::make_shared<Node>(val);
         node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
         if (next)
             next->prev = node;
+        next = node;
     }
 
     void erase() {
-        if (prev)
-            prev->next = next;
+        if (!prev.expired())
+            prev.lock()->next = next;
         if (next)
             next->prev = prev;
     }
 
     ~Node() {
-        printf("~Node()\n");   // 应输出多少次？为什么少了？
+        printf("~Node()\n");   
+        // 应输出多少次？为什么少了？
+        // 13次，未被正常释放
     }
 };
 
@@ -41,14 +41,22 @@ struct List {
     std::shared_ptr<Node> head;
 
     List() = default;
-
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
-        // 请实现拷贝构造函数为 **深拷贝**
+
+        head = std::make_shared<Node>(other.head->value);
+        std::shared_ptr<Node> cur = other.head;
+        while (cur.get()->next) cur = cur.get()->next;
+        while (cur != other.head) {
+            head->insert(cur->value);
+            head->next->prev = head;
+            cur = cur->prev.lock();
+        }
     }
 
-    List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
+    List &operator=(List const &) = delete;  
+    // 为什么删除拷贝赋值函数也不出错？
+    // 使用了拷贝构造函数
 
     List(List &&) = default;
     List &operator=(List &&) = default;
@@ -64,7 +72,7 @@ struct List {
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
+        std::shared_ptr<Node> node = std::make_shared<Node>(value);
         node->next = head;
         if (head)
             head->prev = node;
@@ -72,7 +80,7 @@ struct List {
     }
 
     Node *at(size_t index) const {
-        auto curr = front();
+        Node* curr = front();
         for (size_t i = 0; i < index; i++) {
             curr = curr->next.get();
         }
@@ -80,9 +88,11 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List& lst) {  
+    // 有什么值得改进的？
+    // 引用，减少值拷贝
     printf("[");
-    for (auto curr = lst.front(); curr; curr = curr->next.get()) {
+    for (Node* curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
     }
     printf(" ]\n");
@@ -112,8 +122,8 @@ int main() {
     print(a);   // [ 1 4 2 5 7 ]
     print(b);   // [ 1 4 2 8 5 7 ]
 
-    b = {};
-    a = {};
+    b = List();
+    a = List();
 
     return 0;
 }
