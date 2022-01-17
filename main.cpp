5a69115ec7527d3632ff224be122eb1e6c7a0da7
Wakkana <Wakkana@outlook.com>
2022-01-17 20:17:32 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -4,48 +4,69 @@
 
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    std::unique_ptr<Node> next = nullptr;
+    Node* prev = nullptr;
     // 如果能改成 unique_ptr 就更好了!
 
     int value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
+    explicit Node(int val):value(val) {
     }
 
     void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
+	printf("insert val %d\n", val);
+	auto node = std::make_unique<Node>(val);
+	printf("inital unique\n");
+	if (next) {
+	    next->prev = node.get();
+	    node->next = std::move(next);
+	}
+	printf("after unique\n");
+	node->prev = this;
+	next = std::move(node);
     }
 
     void erase() {
-        if (prev)
-            prev->next = next;
-        if (next)
-            next->prev = prev;
+	if (next)
+	    next->prev = prev;
+	if (prev)
+	    prev->next = std::move(next);
+
     }
 
     ~Node() {
-        printf("~Node()\n");   // 应输出多少次？为什么少了？
+	printf("~Node()\n");   // 应输出多少次？为什么少了？
     }
 };
 
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node> head = nullptr;
 
     List() = default;
 
     List(List const &other) {
-        printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
-        // 请实现拷贝构造函数为 **深拷贝**
+	printf("List 被拷贝！\n");
+	Node* copy = other.front();
+	Node* curr = head.get();
+	printf("obtain copy curr\n");
+	while (copy) {
+	    printf("start ieration\n");
+	    printf("copyvalue = %d\n", copy->value);
+	    if (!curr) {
+		head = std::make_unique<Node>(copy->value);
+		curr = head.get();
+		printf("head\n");
+	    } else {
+		curr->insert(copy->value);
+		curr = curr->next.get();
+		printf("next round\n");
+	    }
+	    copy = copy->next.get();
+	}
+	printf("copy done");
+	// 这是浅拷贝！
+	// 请实现拷贝构造函数为 **深拷贝**
     }
 
     List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
@@ -54,43 +75,45 @@ struct List {
     List &operator=(List &&) = default;
 
     Node *front() const {
-        return head.get();
+	return head.get();
     }
 
     int pop_front() {
-        int ret = head->value;
-        head = head->next;
-        return ret;
+	int ret = head->value;
+	head = std::move(head->next);
+	return ret;
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
-        if (head)
-            head->prev = node;
-        head = node;
+	auto node = std::make_unique<Node>(value);
+	if (head) {
+	    head->prev = node.get();
+	    node->next = std::move(head);
+	}
+	head = std::move(node);
     }
 
     Node *at(size_t index) const {
-        auto curr = front();
-        for (size_t i = 0; i < index; i++) {
-            curr = curr->next.get();
-        }
-        return curr;
+	auto curr = front();
+	for (size_t i = 0; i < index; i++) {
+	    curr = curr->next.get();
+	}
+	return curr;
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List& lst) {  // 有什么值得改进的？
     printf("[");
-    for (auto curr = lst.front(); curr; curr = curr->next.get()) {
-        printf(" %d", curr->value);
+    auto one = lst.front();
+    while (one) {
+	printf(" %d", one->value);
+	one = one->next.get();
     }
     printf(" ]\n");
 }
 
 int main() {
     List a;
-
     a.push_front(7);
     a.push_front(5);
     a.push_front(8);
