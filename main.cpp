aedda185df2c8d3fa74acc6aeb958cd2590c4a39
Sduby <supersduby@gmail.com>
2022-02-08 21:56:57 +0800

    hw02

--- main.cpp
+++ main.cpp
@@ -1,77 +1,98 @@
 /* 基于智能指针实现双向链表 */
 #include <cstdio>
 #include <memory>
+#include <utility>
+#include <type_traits>
 
+template <typename T>
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    // 对于双向链表，相邻项之间存在循环引用，引用计数无法归零释放空间。
+    // 对于双向链表，可以看作前一项“拥有”后一项。
+    // 而后一项保留前一项的Node*指针
+    std::unique_ptr<Node> next;
+    Node* prev;
     // 如果能改成 unique_ptr 就更好了!
 
-    int value;
+    T value;
 
-    // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
-    }
+    // 这个构造函数有什么可以改进的？:value直接根据val构造而不是默认构造后赋值。
+    Node(T val): value(val), prev(nullptr) {}
 
-    void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
-    }
+    // insert会导致无法使用unique_ptr，因为会破环上面假设的“前一项拥有后一项”的前提
+    /*
+              +--- O ---+
+        O ---x           x--- nextO
+              +--- O ---+
+
+        会变成上面这样, 双向链表中用不到该操作所以直接注掉了。
+    */
+    // void insert(int val) {
+    //     auto node = std::make_unique<Node>(val);
+    //     node->next = next;
+    //     node->prev = prev;
+    //     if (prev)
+    //         prev->next = node;
+    //     if (next)
+    //         next->prev = node;
+    // }
 
     void erase() {
-        if (prev)
-            prev->next = next;
         if (next)
             next->prev = prev;
+        if (prev)
+            prev->next = std::move(next);
     }
 
     ~Node() {
-        printf("~Node()\n");   // 应输出多少次？为什么少了？
+        printf("~Node()\n");   // 应输出多少次？为什么少了？因为循环引用
     }
 };
 
+template<typename T>
 struct List {
-    std::shared_ptr<Node> head;
+    std::unique_ptr<Node<T>> head;
 
     List() = default;
 
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        head = std::make_unique<Node<T>>(other.head->value);
+        auto last = head.get();
+        for (auto ptr = other.head->next.get(); ptr; ptr = ptr->next.get()) {
+            auto node = std::make_unique<Node<T>>(ptr->value);
+            node->prev = last;
+            last->next = std::move(node);
+            last = last->next.get();
+        }
         // 请实现拷贝构造函数为 **深拷贝**
     }
 
     List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
+    // 此处拷贝赋值 = 拷贝构造出右值+移动赋值
 
     List(List &&) = default;
     List &operator=(List &&) = default;
 
-    Node *front() const {
+    Node<T> *front() const {
         return head.get();
     }
 
     int pop_front() {
         int ret = head->value;
-        head = head->next;
+        head = std::move(head->next);
         return ret;
     }
 
     void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+        auto node = std::make_unique<Node<T>>(value);
         if (head)
-            head->prev = node;
-        head = node;
+            head->prev = node.get();
+        node->next = std::move(head);
+        head = std::move(node);
     }
 
-    Node *at(size_t index) const {
+    Node<T> *at(size_t index) const {
         auto curr = front();
         for (size_t i = 0; i < index; i++) {
             curr = curr->next.get();
@@ -80,7 +101,7 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List<int> &lst) {  // 有什么值得改进的？: 传入const引用，避免拷贝
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
@@ -89,7 +110,7 @@ void print(List lst) {  // 有什么值得改进的？
 }
 
 int main() {
-    List a;
+    List<int> a;
 
     a.push_front(7);
     a.push_front(5);
@@ -105,7 +126,7 @@ int main() {
 
     print(a);   // [ 1 4 2 8 5 7 ]
 
-    List b = a;
+    List<int> b = a;
 
     a.at(3)->erase();
 
