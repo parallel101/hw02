fc5db27e31e9bda87d65a82d57a10645b1b2d2c9
xiao-ming-hub <3154802372@qq.com>
2023-01-18 16:32:48 +0800

    迭代器支持 调整类方法定义顺序 删点空行

--- main.cpp
+++ main.cpp
@@ -1,29 +1,19 @@
 /* 基于智能指针实现双向链表 */
-#include <cstdio>
+#include <iostream>
 #include <memory>
 
 template <typename Tp> struct Node {
-    // 这两个指针会造成什么问题？请修复。
     std::unique_ptr<Node> next;
     Node *prev;
-    // 如果能改成 unique_ptr 就更好了!
-
     Tp value;
 
-    // 这个构造函数有什么可以改进的？
     Node(Tp val): value(val) {}
+    ~Node() { std::clog << "~Node(" << value << ")\n"; }
 
-    void insert(Tp val) {
-        value = val;
-    }
-
+    void insert(Tp val) { value = val; }
     void erase() {
         if (next) next->prev = prev;
         if (prev) prev->next = std::move(next);
-    }
-
-    ~Node() {
-        printf("~Node()\n");   // 应输出多少次？为什么少了？
     }
 };
 
@@ -31,9 +21,8 @@ template <typename Tp> struct List {
     std::unique_ptr<Node<Tp>> head;
 
     List() = default;
-
     List(List const &other) {
-        printf("List 被拷贝！\n");
+        std::clog << "List 被拷贝！\n";
         // 请实现拷贝构造函数为 **深拷贝**
         if (other.head == nullptr) return;  // 空链表
         Node<Tp> *it = other.front(), *tail;
@@ -44,24 +33,32 @@ template <typename Tp> struct List {
           tail = tail->next.get();
         } 
     }
-
     List &operator=(List const &) = delete; // 为什么删除拷贝赋值函数也不出错？
     // 因为编译器会用“解构函数+拷贝构造”代替拷贝赋值。
-
     List(List &&) = default;
     List &operator=(List &&) = default;
 
-    Node<Tp> *front() const {
-        return head.get();
-    }
+    struct iterator {
+      Node<Tp> *node;
+      iterator(Node<Tp> *p): node(p) {}
 
+      Tp operator*() { return node->value; }
+      iterator operator++() { return node = node->next.get(); }
+      iterator operator--() { return node = node->prev; }
+      bool operator==(iterator const &other) { return node == other.node; }
+      bool operator!=(iterator const &other) { return node != other.node; }
+    };
+    iterator begin() const { return iterator(head.get()); }
+    iterator end() const { return iterator(nullptr); }
+
+    Node<Tp> *front() const { return head.get(); }
+    bool empty() const { return !(head); }
     int pop_front() {
         if (head == nullptr) return 0x114154;
         int ret = head->value;
         head = std::move(head->next);
         return ret;
     }
-
     void push_front(int value) {
         auto node = new Node<Tp>(value);
         if (head)
@@ -69,7 +66,6 @@ template <typename Tp> struct List {
         node->next = std::move(head);
         head = std::unique_ptr<Node<Tp>>(node);
     }
-
     Node<Tp> *at(size_t index) const {
         auto curr = front();
         for (size_t i = 0; i < index; i++) {
@@ -79,15 +75,16 @@ template <typename Tp> struct List {
     }
 };
 
-template <typename Tp> void print(List<Tp> const &lst) {  // 有什么值得改进的？
-    for (auto curr = lst.front(); curr; curr = curr->next.get()) 
-        printf("this: %p prev: %p next: %p value: %lf\n",
-            curr, curr->prev, curr->next.get(), curr->value);
+template <typename Tp>
+std::ostream &operator<<(std::ostream &os, List<Tp> const &l) {
+  for (os << '['; Tp x : l) os << x << ", ";
+  os << (l.empty() ? "]" : "\b\b]");
+  return os;
 }
 
 int main() {
+#define print(l) std::cout << l << '\n';
     List<int> a;
-
     a.push_front(7);
     a.push_front(5);
     a.push_front(8);
@@ -95,22 +92,14 @@ int main() {
     a.push_front(9);
     a.push_front(4);
     a.push_front(1);
-
     print(a);   // [ 1 4 9 2 8 5 7 ]
-
     a.at(2)->erase();
-
     print(a);   // [ 1 4 2 8 5 7 ]
-
     List<int> b = a;
-
     a.at(3)->erase();
-
     print(a);   // [ 1 4 2 5 7 ]
     print(b);   // [ 1 4 2 8 5 7 ]
-
     b = {};
     a = {};
-
     return 0;
 }
