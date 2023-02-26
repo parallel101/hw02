e3e41d646033b6e50f312a5f697e9d548291975f
M-Cyclone <1150235785@qq.com>
2023-02-26 22:37:54 +0800

    2023-02-26 -将Node实现为unique_ptr的形式，并设置为模板类 -调整List的函数实现

--- main.cpp
+++ main.cpp
@@ -1,95 +1,122 @@
 /* 基于智能指针实现双向链表 */
 #include <cstdio>
 #include <memory>
+#include <iostream>
 
-struct Node {
+template <typename T>
+struct Node
+{
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    std::unique_ptr<Node> next;
+    Node*                 prev;
     // 如果能改成 unique_ptr 就更好了!
 
-    int value;
+    T value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
-    }
+    explicit Node(T val) : value{ std::move(val) } {}
 
-    void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
+    void insert(T val)
+    {
+        auto node = std::make_unique<Node>(std::move(val));
+
         node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
+        if (next) next->prev = node.get();
+
+        node->next = std::move(next);
+        if (prev) prev->next = std::move(node);
     }
 
-    void erase() {
-        if (prev)
-            prev->next = next;
-        if (next)
-            next->prev = prev;
+    void erase()
+    {
+        if (next) next->prev = prev;
+        if (prev) prev->next = std::move(next);
     }
 
-    ~Node() {
-        printf("~Node()\n");   // 应输出多少次？为什么少了？
+    ~Node()
+    {
+        printf("~Node()\n");  // 应输出多少次？为什么少了？
     }
 };
 
-struct List {
-    std::shared_ptr<Node> head;
+template <typename T>
+struct List
+{
+    std::unique_ptr<Node<T>> head;
 
     List() = default;
 
-    List(List const &other) {
+    List(const List& other)
+    {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        //head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
+
+        auto store = std::make_unique<Node<T>>(T{});
+        auto ptr   = other.front();
+        auto res   = store.get();
+
+        while (ptr)
+        {
+            auto node  = std::make_unique<Node<T>>(ptr->value);
+            node->prev = res->prev;
+            node->next = std::move(res->next);
+            res->next  = std::move(node);
+            res        = res->next.get();
+            ptr        = ptr->next.get();
+        }
+
+        head = std::move(store->next);
+        head->prev = nullptr;
     }
 
-    List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
+    List& operator=(const List&) = delete;  // 为什么删除拷贝赋值函数也不出错？
 
-    List(List &&) = default;
-    List &operator=(List &&) = default;
+    List(List&&)            = default;
+    List& operator=(List&&) = default;
 
-    Node *front() const {
-        return head.get();
-    }
+    Node<T>* front() const { return head.get(); }
 
-    int pop_front() {
-        int ret = head->value;
-        head = head->next;
+    T pop_front()
+    {
+        T ret = std::move(head->value);
+        head  = std::move(head->next);
         return ret;
     }
 
-    void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
-        if (head)
-            head->prev = node;
-        head = node;
+    void push_front(T value)
+    {
+        auto node = std::make_unique<Node<T>>(std::move(value));
+        if (head) head->prev = node.get();
+        node->next = std::move(head);
+        head       = std::move(node);
     }
 
-    Node *at(size_t index) const {
+    Node<T>* at(size_t index) const
+    {
         auto curr = front();
-        for (size_t i = 0; i < index; i++) {
+        for (size_t i = 0; i < index && curr; i++)
+        {
             curr = curr->next.get();
         }
         return curr;
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
-    printf("[");
-    for (auto curr = lst.front(); curr; curr = curr->next.get()) {
-        printf(" %d", curr->value);
+template <typename T>
+void print(const List<T>& lst)
+{  // 有什么值得改进的？
+    std::cout << "[";
+    for (auto curr = lst.front(); curr; curr = curr->next.get())
+    {
+        std::cout << " " << curr->value;
     }
-    printf(" ]\n");
+    std::cout << " ]" << std::endl;
 }
 
-int main() {
-    List a;
+int main()
+{
+    List<int> a;
 
     a.push_front(7);
     a.push_front(5);
@@ -99,18 +126,18 @@ int main() {
     a.push_front(4);
     a.push_front(1);
 
-    print(a);   // [ 1 4 9 2 8 5 7 ]
+    print(a);  // [ 1 4 9 2 8 5 7 ]
 
     a.at(2)->erase();
 
-    print(a);   // [ 1 4 2 8 5 7 ]
+    print(a);  // [ 1 4 2 8 5 7 ]
 
     List b = a;
 
     a.at(3)->erase();
 
-    print(a);   // [ 1 4 2 5 7 ]
-    print(b);   // [ 1 4 2 8 5 7 ]
+    print(a);  // [ 1 4 2 5 7 ]
+    print(b);  // [ 1 4 2 8 5 7 ]
 
     b = {};
     a = {};
